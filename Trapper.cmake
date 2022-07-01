fe45ffcb276df546c162f0cef7aafb523a06c8b7
kabu <kabukunz@gmail.com>
2022-07-01 13:04:24 +0200

    fixing source dir

--- Trapper.cmake
+++ Trapper.cmake
@@ -324,19 +324,21 @@ function(trapper_add_package PACKAGE LOCATION HASHING)
     string(FIND ${TRAPPER_LOCATION_EXT} "zip" IS_ZIP)
     string(FIND ${TRAPPER_LOCATION_EXT} "git" IS_GIT)
 
-    if((IS_TAR GREATER_EQUAL 0) OR (IS_GZIP GREATER_EQUAL 0) OR (IS_ZIP GREATER_EQUAL 0))
-        # zip
-        set(TRAPPER_LOCATION "URL \"${TRAPPER_LOCATION}\"")
-        if(NOT TRAPPER_HASHING)
-            set(TRAPPER_HASHING "")
-        else()
-            string(HEX ${TRAPPER_HASHING} hex_hashing)
-            set(TRAPPER_HASHING "URL_MD5 ${hex_hashing}")
+    if(TRAPPER_LOCATION_EXT)
+        if((IS_TAR GREATER_EQUAL 0) OR (IS_GZIP GREATER_EQUAL 0) OR (IS_ZIP GREATER_EQUAL 0))
+            # zip
+            set(TRAPPER_LOCATION "URL \"${TRAPPER_LOCATION}\"")
+            if(NOT TRAPPER_HASHING)
+                set(TRAPPER_HASHING "")
+            else()
+                string(HEX ${TRAPPER_HASHING} hex_hashing)
+                set(TRAPPER_HASHING "URL_MD5 ${hex_hashing}")
+            endif()
+        else((IS_GIT GREATER_EQUAL 0))
+            # git
+            set(TRAPPER_LOCATION "GIT_REPOSITORY \"${TRAPPER_LOCATION}\"")
+            set(TRAPPER_HASHING "GIT_TAG ${TRAPPER_HASHING}")
         endif()
-    elseif((IS_GIT GREATER_EQUAL 0))
-        # git
-        set(TRAPPER_LOCATION "GIT_REPOSITORY \"${TRAPPER_LOCATION}\"")
-        set(TRAPPER_HASHING "GIT_TAG ${TRAPPER_HASHING}")
     else()
         # non-empty source dir
         set(TRAPPER_SOURCE_DIR_COMMAND "SOURCE_DIR \"${TRAPPER_LOCATION}\"")
